f243f6fdf8d55fb375fa7b02f06ca0b4a0d497ce
ekyeremeh7 <ekyeremeh7@gmail.com>
2025-11-01 21:50:23 +0000

    Gradle changes

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/emmanuelkyeremeh/development/flutter"
+export "FLUTTER_ROOT=/Users/emmanuelkyeremeh/Development/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/emmanuelkyeremeh/Projects/code_and_cocktails/code_and_cocktails"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
